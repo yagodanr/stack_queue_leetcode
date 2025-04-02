963e4886fa3c8828b9184c605477bee53cfcddae
yagodanr <yagodanr@gmail.com>
2025-04-02 09:46:30 +0300

    Implement Queue using Stacks

--- Implement Queue using Stacks/main.py
+++ Implement Queue using Stacks/main.py
@@ -7,16 +7,32 @@ class MyQueue:
         self.sto = queue.LifoQueue()
 
     def push(self, x: int) -> None:
+        if self.sti.empty():
+            while not self.sto.empty():
+                self.sti.put(self.sto.get())
 
+        self.sti.put(x)
 
     def pop(self) -> int:
+        if self.sto.empty():
+            while not self.sti.empty():
+                self.sto.put(self.sti.get())
+
+        return self.sto.get()
 
 
     def peek(self) -> int:
+        if self.sto.empty():
+            while not self.sti.empty():
+                self.sto.put(self.sti.get())
+
+        tmp = self.sto.get()
+        self.sto.put(tmp)
+        return tmp
 
 
     def empty(self) -> bool:
-
+        return self.sto.empty() and self.sti.empty()
 
 
 # Your MyQueue object will be instantiated and called as such:
