8df380404aee0d7142e1ce94c0237634a1eac568
yagodanr <yagodanr@gmail.com>
2025-04-02 12:36:22 +0300

    Implement Stack using Queues

--- Implement Stack using Queues/main.py
+++ Implement Stack using Queues/main.py
@@ -7,21 +7,46 @@ class MyStack:
         self.q2 = queue.Queue()
 
     def push(self, x: int) -> None:
+        # self.q2.put(x)
+        # while not self.q.empty():
+        #     self.q2.put(self.q.get())
 
+        # self.q, self.q2 = self.q2, self.q
+
+        self.q.put(x)
 
     def pop(self) -> int:
+        if self.q.empty():
+            return None
+
+        prev = None
+        while True:
+            prev = self.q.get()
+            if self.q.empty():
+                self.q, self.q2 = self.q2, self.q
+                return prev
+            self.q2.put(prev)
+
 
 
     def top(self) -> int:
+        prev = None
+        while not self.q.empty():
+            prev = self.q.get()
+            self.q2.put(prev)
+
+        self.q, self.q2 = self.q2, self.q
+        return prev
 
 
     def empty(self) -> bool:
-
+        return self.q.empty()
 
 
 # Your MyStack object will be instantiated and called as such:
-# obj = MyStack()
-# obj.push(x)
-# param_2 = obj.pop()
-# param_3 = obj.top()
-# param_4 = obj.empty()
+obj = MyStack()
+obj.push(1)
+obj.push(2)
+print(obj.top())
+print(obj.pop())
+print(obj.empty())
