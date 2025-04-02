3eb67154371d693ca7b072a289d20534aaf04645
yagodanr <yagodanr@gmail.com>
2025-04-02 13:15:47 +0300

    Not tested yet

--- Implement Stack using Queues/Maximum Frequency Stack/main.py
+++ Implement Stack using Queues/Maximum Frequency Stack/main.py
@@ -4,34 +4,54 @@ class FreqStack:
 
     def __init__(self):
         self.s = LifoQueue()
-        self.s1 = LifoQueue()
 
 
     def push(self, val: int) -> None:
-
-        present = False
-        while not self.s.empty():
-            st: LifoQueue = self.s.get()
-            top = st.pop()
-            if top == val:
-                st.put(val)
-                present = True
-
-            self.s1.put(st)
-
-        if not present:
-            tmp = LifoQueue()
-            tmp.put(val)
-            self.s1.put(tmp)
-        self.s, self.s1 = self.s1, self.s
-
+        self.s.put(val)
 
 
     def pop(self) -> int:
+        tmp = LifoQueue()
+        tmp2 = LifoQueue()
 
+        def search_add(val):
+            nonlocal tmp, tmp2
+            while not tmp.empty():
+                top = tmp.get()
+                if top[0] == val:
+                    while not tmp2.empty():
+                        tmp.put(tmp2.get())
+                    tmp.put((val, top[1]+1))
+                    break
+                tmp2.put(top)
+            else:
+                while not tmp2.empty():
+                    tmp.put(tmp2.get())
+                    tmp.put((val, 1))
+
+        s2 = LifoQueue()
+        while not self.s.empty():
+            val = self.s.get()
+            search_add(val)
+            s2.put(val)
+        while not s2.empty():
+            self.s.put(s2.get())
+        return tmp.get()[0]
 
 
 # Your FreqStack object will be instantiated and called as such:
 # obj = FreqStack()
 # obj.push(val)
 # param_2 = obj.pop()
+
+freqStack = FreqStack();
+freqStack.push(5) # The stack is [5]
+freqStack.push(7) # The stack is [5,7]
+freqStack.push(5) # The stack is [5,7,5]
+freqStack.push(7) # The stack is [5,7,5,7]
+freqStack.push(4) # The stack is [5,7,5,7,4]
+freqStack.push(5) # The stack is [5,7,5,7,4,5]
+print(freqStack.pop())   # return 5, as 5 is the most frequent. The stack becomes [5,7,5,7,4].
+print(freqStack.pop())   # return 7, as 5 and 7 is the most frequent, but 7 is closest to the top. The stack becomes [5,7,5,4].
+print(freqStack.pop())   # return 5, as 5 is the most frequent. The stack becomes [5,7,4].
+print(freqStack.pop())   # return 4, as 4, 5 and 7 is the most frequent, but 4 is closest to the top. The stack becomes [5,7].
