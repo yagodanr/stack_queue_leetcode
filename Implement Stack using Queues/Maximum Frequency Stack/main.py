b5c23f117e047d9141dee3e9488d9afc3ad6de99
yagodanr <yagodanr@gmail.com>
2025-04-02 13:02:04 +0300

    Tried push

--- Implement Stack using Queues/Maximum Frequency Stack/main.py
+++ Implement Stack using Queues/Maximum Frequency Stack/main.py
@@ -1,9 +1,30 @@
+from queue import LifoQueue
+
 class FreqStack:
 
     def __init__(self):
+        self.s = LifoQueue()
+        self.s1 = LifoQueue()
 
 
     def push(self, val: int) -> None:
+
+        present = False
+        while not self.s.empty():
+            st: LifoQueue = self.s.get()
+            top = st.pop()
+            if top == val:
+                st.put(val)
+                present = True
+
+            self.s1.put(st)
+
+        if not present:
+            tmp = LifoQueue()
+            tmp.put(val)
+            self.s1.put(tmp)
+        self.s, self.s1 = self.s1, self.s
+
 
 
     def pop(self) -> int:
